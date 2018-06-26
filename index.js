f541002426cb14d28a134668af5d769ded0a6513
Pippirincho <40601373+Pippirincho@users.noreply.github.com>
2018-06-26 12:47:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-<script>
 $(document).ready(function(){
-  $(button).click(function(){
+  $("button").click(function(){
     alert("Hello, welcome to my page");
     $("iframe").hide();
   });
 })
 
-</script>
+
